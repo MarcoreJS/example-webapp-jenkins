fed3e54e57cebbe3e8335fcd4ecb6d82f5ec04f8
Marco Mercado Lugo <marcomercadolugo@MacBook-Pro-de-Marco.local>
2018-10-04 10:06:33 -0500

    adding ecs deploy fix

--- run-stack.sh
+++ run-stack.sh
@@ -7,6 +7,6 @@ aws cloudformation $ACTION \
     --capabilities CAPABILITY_NAMED_IAM \
     --parameters \
     ParameterKey=DockerImage,ParameterValue=805495149875.dkr.ecr.us-east-1.amazonaws.com/example-webapp:$(git rev-parse HEAD) \
-    ParameterKey=VPC,ParameterValue=vpc-9a4a99e0
+    ParameterKey=VPC,ParameterValue=vpc-9a4a99e0 \
     ParameterKey=Cluster,ParameterValue=default \
     ParameterKey=Listener,ParameterValue=arn:arn:aws:elasticloadbalancing:us-east-1:805495149875:listener/app/production-website/3d9cb6bfdd5d4de5/16871d016b579d3a
