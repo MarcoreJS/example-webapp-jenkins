07929c69768b8cf20389eeb40c12e62eb5859a59
Marco Mercado Lugo <marcomercadolugo@MacBook-Pro-de-Marco.local>
2018-10-04 09:49:32 -0500

    adding ecs deploy

---
+++ run-stack.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+aws cloudformation $ACTION \
+    --region us-east-1 \
+    --stack-name $STACK_NAME \
+    --template-body file://service.yaml \
+    --capabilities CAPABILITY_NAMED_IAM \
+    --parameters \
+    ParameterKey=DockerImage,ParameterValue=805495149875.dkr.ecr.us-east-1.amazonaws.com/example-webapp:$(git rev-parse HEAD) \
+    ParameterKey=VPC,ParameterValue=vpc-9a4a99e0
+    ParameterKey=Cluster,ParameterValue=default \
+    ParameterKey=Listener,ParameterValue=arn:arn:aws:elasticloadbalancing:us-east-1:805495149875:listener/app/production-website/3d9cb6bfdd5d4de5/16871d016b579d3a
